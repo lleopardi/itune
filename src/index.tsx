7bfea9d281d7f45639a558e98044b24fe2761c71
Luis Leopardi <leopardi.luis@gmail.com>
2023-04-13 23:36:34 -0500

    feat(app): add route with podcast component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,36 @@
 import "./index.scss";
 
 import React, { StrictMode } from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import App from "./views/App";
+import Home from "./views/Home";
 import { createRoot } from "react-dom/client";
+import Podcast from "./views/Podcast";
+import getEpisodesByPodcastId from "./services/episodes.service";
 
-const Index = () => <div>Hello world</div>
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />}></Route>
+      <Route
+        path="podcast/:id"
+        element={<Podcast />}
+        loader={getEpisodesByPodcastId}
+      ></Route>
+    </Route>
+  )
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <StrictMode>
-    <Index/>
+    <RouterProvider router={router} />
   </StrictMode>
 );
